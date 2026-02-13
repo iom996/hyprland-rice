8a50b071f8a96a8893658d9cdb3f12eded424199
iomix <iomix@tuta.io>
2026-02-13 17:02:25 +0100

    Refactor install script for efficiency and clarity
    Simplified multiple mv commands into a single command and updated the success message.

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ clear
 cd ~
 git clone https://github.com/Norton42qq/hyprland-rice/
 cd hyprland-rice
-mv hypr/ ~/.config/ && mv waybar/ ~/.config/ && mv wofi/ ~/.config/ && mv alacritty/ ~/.config/ && mv dunst/ ~/.config && mv .zshrc ~/
+mv {hypr,waybar,wofi,alacritty,dunst} ~/.config/ && mv .zshrc ~/
 rm -rf ~/hyprland-rice
 clear
-echo "райс успешно установлен. Пожалуйста, навсякий случай перезапустите сессию hyprland"
+echo "райс успешно установлен. перезапусти сессию hyprland"
